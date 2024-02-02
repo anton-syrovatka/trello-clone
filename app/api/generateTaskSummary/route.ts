171c7657e8e15ba1740b68d7635102e1f5b46849
Anton Syrovatka <syrovatka.anton@gmail.com>
2024-02-02 11:39:11 +0100

    openai

--- app/api/generateTaskSummary/route.ts
+++ app/api/generateTaskSummary/route.ts
@@ -1,32 +1,32 @@
-import openai from "@/openai";
-import { NextResponse } from "next/server";
+import openai from '@/openai';
+import { NextResponse } from 'next/server';
 
 export async function POST(request: Request) {
   const { tasks } = await request.json();
 
-  if (process.env.OPENAI_ENABLED! !== "true") {
+  if (process.env.OPENAI_ENABLED! !== 'true') {
     return NextResponse.json({
       content:
-        "Welcome to Trello Clone! \nUnfortunately AI support is not available at the moment.",
+        'Welcome to Trello Clone! \nUnfortunately AI support is not available at the moment.',
     });
   }
 
   try {
     const completion = await openai.chat.completions.create({
-      model: "gpt-3.5-turbo",
+      model: 'gpt-3.5-turbo',
       temperature: 0.8,
       n: 1,
       stream: false,
       messages: [
         {
-          role: "system",
+          role: 'system',
           content: `
             When responding, welcome user and say welcome to Trello Clone!
             Limit the response to 200 characters
           `,
         },
         {
-          role: "user",
+          role: 'user',
           content: `
             Hi there, provide a summary of the following tasks.
             Count how many tasks are in each category such as "Todo", "In Progress", "Done".
@@ -42,6 +42,7 @@ export async function POST(request: Request) {
 
     return NextResponse.json(choice.message);
   } catch (error) {
-    return NextResponse.json(error);
+    return NextResponse.json({ error });
+    // return NextResponse.json({ error }, { status: 400 });
   }
 }
